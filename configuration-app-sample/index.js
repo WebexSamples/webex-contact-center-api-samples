2000f159535e0a887d18b514c9535edd8f80506b
Niko Theo <niko.th@gmail.com>
2022-03-08 13:19:43 -0800

    init

---
+++ configuration-app-sample/index.js
@@ -0,0 +1,79 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+
+//needed for CommonJS
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// oauth info
+const clientID = process.env.CLIENT_ID; // make sure to add your IDs in your own .env file
+const clientSecret = process.env.CLIENT_SECRET; // make sure to add your IDs in your own .env file
+
+const app = express();
+app.use(cors());
+
+//Declare the redirect route
+app.get("/oauth/redirect", (req, res) => {
+  // The req.query object has the query params that
+  // were sent to this route. We want the `code` param
+  const requestToken = req.query.code;
+
+  async function getAccessToken() {
+    // let data = stringify({});
+    let data = {};
+    let config = {
+      method: "post",
+      url: `https://webexapis.com/v1/access_token?grant_type=authorization_code&client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}&redirect_uri=https://oauth-webex.herokuapp.com/oauth/redirect`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: data
+    };
+    axios(config)
+      .then(function (response) {
+        const accessToken = response.data.access_token;
+        const refreshToken = response.data.refresh_token;
+        // redirect the user to the welcome page, along with the access token
+        getRefreshToken(refreshToken);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  async function getRefreshToken(refreshToken) {
+    let data = {};
+    let config = {
+      method: "post",
+      url: `https://webexapis.com/v1/access_token?grant_type=refresh_token&client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&redirect_uri=https://oauth-webex.herokuapp.com/oauth/redirect`,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: data
+    };
+    axios(config)
+      .then(function (response) {
+        const accessToken = response.data.access_token;
+        console.log(`get from Refresh token ${JSON.stringify(response.data)}`);
+        // redirect the user to the welcome page, along with the access token
+        res.redirect(`/app.html?access_token="${accessToken}"`);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  getAccessToken();
+});
+
+app.get("/", (req, res) => {
+  res.redirect("/app.html");
+});
+
+app.use(express.static(__dirname + "/src/public"));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("listening on port 3000");
+});
---
+++ configuration-app-sample/src/public/js/app.js
@@ -0,0 +1,105 @@
+import { buildForm } from "./helpers/buildForm.js";
+import { formError } from "./helpers/formError.js";
+
+//build form
+buildForm();
+
+//get Access token
+const token = getBearerToken();
+
+function getBearerToken() {
+  try {
+    const query = decodeURI(window.location.search.substring(1));
+    const accessToken = query.split("access_token=")[1];
+    console.log(accessToken);
+    localStorage.setItem("token", accessToken);
+    return JSON.parse(accessToken);
+  } catch (error) {
+    location.href = "http://localhost:3000/index.html";
+  }
+}
+
+let getResults = document.getElementById("submit");
+getResults.addEventListener("click", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  // add error down at Footer if fields are not complete
+  let areaMessage = document.getElementById("pageFooter");
+  areaMessage.addEventListener("click", e => {
+    let arr = areaMessage.childNodes;
+    let newArr = Array.from(arr);
+    newArr[1].remove();
+    formError();
+  });
+
+  //form values
+  let selectMethod = document.getElementById("selectMethod").value;
+  let org = document.getElementById("org").value;
+  let endpoint = document.getElementById("endpoint").value;
+  let page = document.getElementById("page").value;
+  let pageSize = document.getElementById("pagesize").value;
+  let body = document.getElementById("body").value;
+
+  //makes sure form is filled out
+  if (selectMethod === "" || org === "" || endpoint === "") {
+    let selectMethod = document.getElementById("selectMethod");
+    selectMethod.style.borderColor = "red";
+    let org = document.getElementById("org");
+    org.style.borderColor = "red";
+    let endpoint = document.getElementById("endpoint");
+    endpoint.style.borderColor = "red";
+    let formError = document.getElementById("pageFooter");
+    formError.innerHTML = `
+      <h2 id="message" class="message"> Please fill required Fields </h2>
+    `;
+    return;
+  } else {
+    formError();
+  }
+
+  const url = `https://api.wxcc-us1.cisco.com/organization/${org}/${endpoint}?page=${page}&pageSize=${pageSize}`;
+
+  fetch(url, {
+    method: `${selectMethod}`,
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => response.json())
+    .then(result => {
+      // console.log(result);
+      let res = result;
+      let results = res.map(el => {
+        let email = el.email;
+        let getElements = el.name;
+        if (endpoint === "team" || endpoint === "skill-profile" || endpoint === "contact-service-queue" || endpoint === "entry-point") {
+          return `
+  					<option>${getElements}</option>
+  					`;
+        } else if (endpoint === "user") {
+          return `
+  					<option>${email}</option>
+  					`;
+        }
+      });
+      const apiResult = document.getElementById("mainArticle");
+      apiResult.innerHTML += `
+  				<div class="card">
+  					<select>
+  						<option>${endpoint} list</option>
+  						${results}
+  					</select>
+  				</div>
+  			`;
+    })
+    .catch(function (error) {
+      console.log(error);
+      // location.href = "https://oauth-webex.herokuapp.com/index.html";
+      location.href = "http://localhost:3000/index.html";
+    });
+
+  {
+  }
+}
