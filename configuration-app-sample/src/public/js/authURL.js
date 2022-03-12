dc5cc5f424d3f5f23b23039238b85a326e226832
Niko Theo <niko.th@gmail.com>
2022-03-11 20:18:59 -0800

    stage 4 working

--- configuration-app-sample/src/public/js/authURL.js
+++ configuration-app-sample/src/public/js/authURL.js
@@ -1,9 +1,17 @@
+const oauth = {
+  client_id: "C239e8b22acfb54145054c7626e1cdf3fe6b6c8d2a80562c0edfcc2f7e8e27889",
+  redirect_uri: "http://localhost:5000/oauth/redirect",
+  scope1: "cjp:config",
+  scope2: "cjp:config_write",
+  scope3: "cjp:config_read"
+};
+
 //build auth URL
 const div = document.querySelector("#mainArticle");
 const authBtn = document.createElement("a");
 authBtn.classList.add("btn");
 authBtn.textContent = "WxCC oAuth";
-authBtn.href = "https://webexapis.com/v1/authorize?client_id=C239e8b22acfb54145054c7626e1cdf3fe6b6c8d2a80562c0edfcc2f7e8e27889&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Foauth%2Fredirect&scope=cjp%3Aconfig_write%20cjp%3Aconfig%20cjp%3Aconfig_read&state=set_state_here";
+authBtn.href = `https://webexapis.com/v1/authorize?client_id=${oauth.client_id}&response_type=code&redirect_uri=${oauth.redirect_uri}&scope=${oauth.scope2} ${oauth.scope1} ${oauth.scope3}&state=set_state_here`;
 div.appendChild(authBtn);
 
 console.log(div);
