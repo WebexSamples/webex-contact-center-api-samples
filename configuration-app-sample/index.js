680aa26a725232cd854134fd64ac020d9c825ad4
Niko Theo <niko.th@gmail.com>
2022-03-11 16:21:38 -0800

    add GET dotenv

--- configuration-app-sample/index.js
+++ configuration-app-sample/index.js
@@ -9,19 +9,13 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// oauth info
-const clientID = process.env.CLIENT_ID; // make sure to add your IDs in your own .env file
-const clientSecret = process.env.CLIENT_SECRET; // same as above
-const port = process.env.PORT;
-const redirect = process.env.REDIRECT;
-
 // middleware
 const app = express();
 app.use(cors());
 app.use(express.static(__dirname + "/src/public"));
 
 // Declare the redirect route
-app.get("/oauth/redirect", async (req, res) => {
+app.get(process.env.REDIRECT_URI_GET, async (req, res) => {
   // The req.query object has the query params that
   // were sent to this route. We want the `code` param
   const code = req.query.code;
