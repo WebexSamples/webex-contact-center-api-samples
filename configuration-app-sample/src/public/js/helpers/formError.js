6420252119b66c4191e18abb3d6f035237c2e4fc
Niko Theo <niko.th@gmail.com>
2022-03-13 15:41:33 -0700

    traverse error back to gray

--- configuration-app-sample/src/public/js/helpers/formError.js
+++ configuration-app-sample/src/public/js/helpers/formError.js
@@ -1,12 +1,12 @@
 import { select } from "../sa-lib/select.js";
 
 export function formError() {
-  let selectMethod = select("#selectMethod");
-  selectMethod.style.borderColor = "#ccc";
-  let org = select("#org");
-  org.style.borderColor = "#ccc";
-  let endpoint = select("#endpoint");
-  endpoint.style.borderColor = "#ccc";
-  let footer = select("#pageFooter");
-  footer.innerHTML = "";
+  let color = select("#form").childNodes;
+  color.forEach(element => {
+    if (element.classList.contains("mandatory")) {
+      element.style.borderColor = "#ccc";
+    }
+    let footer = select("#pageFooter");
+    footer.innerHTML = "";
+  });
 }
