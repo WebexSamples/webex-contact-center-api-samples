dfb7ef3647354b5818b712403dff10d8a67e8b9c
Niko Theo <niko.th@gmail.com>
2022-03-13 11:36:43 -0700

    add select custom method

--- configuration-app-sample/src/public/js/app.js
+++ configuration-app-sample/src/public/js/app.js
@@ -2,6 +2,8 @@
 import { buildForm } from "./helpers/buildForm.js";
 import { formError } from "./helpers/formError.js";
 import { getBearerToken } from "./helpers/getBearerToken.js";
+// use this in place DOM "getElementById", etc...
+import { select } from "./sa-lib/select.js";
 
 //build form
 buildForm();
@@ -12,14 +14,14 @@ const token = getBearerToken();
 //get Bearer
 getBearerToken();
 
-let getResults = document.getElementById("submit");
+let getResults = select("#submit");
 getResults.addEventListener("click", submitForm);
 
 function submitForm(e) {
   e.preventDefault();
 
   // add error down at Footer if fields are not complete
-  let areaMessage = document.getElementById("pageFooter");
+  let areaMessage = select("#pageFooter");
   areaMessage.addEventListener("click", e => {
     let arr = areaMessage.childNodes;
     let newArr = Array.from(arr);
@@ -28,22 +30,22 @@ function submitForm(e) {
   });
 
   //form values
-  let selectMethod = document.getElementById("selectMethod").value;
-  let org = document.getElementById("org").value;
-  let endpoint = document.getElementById("endpoint").value;
-  let page = document.getElementById("page").value;
-  let pageSize = document.getElementById("pagesize").value;
-  let body = document.getElementById("body").value;
+  let selectMethod = select("#selectMethod").value;
+  let org = select("#org").value;
+  let endpoint = select("#endpoint").value;
+  let page = select("#page").value;
+  let pageSize = select("#pagesize").value;
+  let body = select("#body").value;
 
   //makes sure form is filled out
   if (selectMethod === "" || org === "" || endpoint === "") {
-    let selectMethod = document.getElementById("selectMethod");
+    let selectMethod = select("#selectMethod");
     selectMethod.style.borderColor = "red";
-    let org = document.getElementById("org");
+    let org = select("#org");
     org.style.borderColor = "red";
-    let endpoint = document.getElementById("endpoint");
+    let endpoint = select("#endpoint");
     endpoint.style.borderColor = "red";
-    let formError = document.getElementById("pageFooter");
+    let formError = select("#pageFooter");
     formError.innerHTML = `
       <h2 id="message" class="message"> Please fill required Fields </h2>
     `;
@@ -75,7 +77,7 @@ function submitForm(e) {
   					`;
         }
       });
-      const apiResult = document.getElementById("mainArticle");
+      const apiResult = select("#mainArticle");
       apiResult.innerHTML += `
   				<div class="card">
   					<select>
