b5a1543d7c68913dbb0e856cea7ac778484dabc5
Niko Theo <niko.th@gmail.com>
2022-04-28 13:29:19 -0700

    Add New Sample to Repo - Widget Sample 101

---
+++ widget-sample-101/README.md
@@ -0,0 +1,56 @@
+## Desktop Widget 101 - Sample Widget
+
+Simple vanilla JS Web Component desktop widget showing ability to pass data into the widget.
+Watch the demo below to understand what this app does and how to set it up and extend on it.
+
+## [Watch Now: Configuration Widget Sample Overview and Demo](https://app.vidcast.io/share/25878069-6460-4a18-aaef-8e639efc48d3)
+
+## Getting Started
+
+This app is assuming you already have a Client ID and Secret. If you don't please go to [postman-sample app](https://github.com/CiscoDevNet/webex-contact-center-api-samples/tree/main/postman-sample) and obtain your Client ID and Client Secret.
+
+### Executing program
+
+How to run the app:
+
+- **Step 1:**
+
+  - Inside this project on your terminal type: `npm run dev`
+  - this should run the app on your localhost:5000
+
+- **Step 2:**
+
+  - Upload the **_widgetStoreSample.json_** file onto your Tenant Portal **[WebexCC Portal](https://portal-v2.wxcc-us1.cisco.com/portal/home.html#)**
+    - _link above is referencing US portal please change if you are in different geo_
+  - Log in to your agent to view the new layout.
+
+- **Step 3:**
+  - The CAD variables used in this demo are specific to a different tenant than what you will have. You will have to modify the some if this information.
+
+## Useful Links
+
+**[Desktop Guide](https://developer.webex-cx.com/documentation/guides/desktop)**
+**[Desktop Layout](https://www.cisco.com/c/en/us/td/docs/voice_ip_comm/cust_contact/contact_center/webexcc/SetupandAdministrationGuide_2/b_mp-release-2/b_cc-release-2_chapter_011.html#topic_8230815F4023699032326F948C3F1495)**
+
+## Disclaimer
+
+> These samples are meant to be used, as "samples", for demos, and to understand how to interact w
+> ith the WebexCC APIs.
+> When building a production grade solution, please consider the overall architecture and design with a security first approach.
+> Also, please consider how you would extend this app for multiple orgs, manage tokens for the orgs, etc.
+> These samples are only meant to provide working, starter code and many layers have been simplified and abstracted away to focus on the Webex Contact Center use cases.
+
+## Support
+
+For Support and Assistance, use the Cisco Developer Community Page:
+
+Need Help? Visit the **[Webex Contact Center APIs Developer Community](https://community.cisco.com/t5/contact-center/bd-p/j-disc-dev-contact-center)**
+
+Refer: **[How to Ask a Question or Initiate a Discussion](https://community.cisco.com/t5/contact-center/webex-contact-center-apis-developer-community-and-support/m-p/4558270)**
+
+## Version History
+
+- 1.0.0
+  - a few GET methods & updated readme
+    <!-- * See [commit change]() or See [release history]() -->
+    <!-- * See [commit change]() or See [release history]() -->
---
+++ widget-sample-101/src/widget-store-sample.js
@@ -0,0 +1,230 @@
+/* Adding shadowDOM to a Web Component
+			Step 1: attachShadow
+			Step 2: create a const template
+			Step 3: attach template to innerHTML
+			Step 4: attach to document
+*/
+
+export default class InfoCard extends HTMLElement {
+  constructor() {
+    super();
+    // **STEP (1)** //
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  // Using $Store key to iterate over object and get a specific CAD value
+  myFunc() {
+    let info = this.cad;
+    for (const iterator of info) {
+      return iterator[1].interaction.callAssociatedData.customerID.value;
+    }
+  }
+
+  // Using $Store key to iterate over the media object from the TaskMap Map/Object
+  mediaInfo() {
+    const info = this.cad;
+    for (const iterator of info) {
+      const media = iterator[1].interaction.media;
+      const response = Object.keys(media).map(info => {
+        return media[info];
+      });
+      return response;
+    }
+  }
+
+  // Using $Store key to iterate over the media object and grab the participants
+  activeParticipants() {
+    // pull out your specific objects
+    const info = this.cad;
+    for (const iterator of info) {
+      const media = iterator[1].interaction.media;
+      const response = Object.keys(media).map(info => {
+        const { participants } = media[info];
+        return participants;
+      });
+      return response;
+    }
+  }
+
+  //Render on the DOM
+  render() {
+    // **STEP (2)** //
+    const template = document.createElement("template");
+
+    // **STEP (3)** //
+    template.innerHTML = `
+		<style>
+				.container {
+					overflow: var(--flow, scroll);
+					background: var(--back, inherit)
+				}
+				.cards {
+					display: grid;
+					grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+					grid-auto-rows: auto;
+					grid-gap: 1rem;
+				}
+					h1 {
+					text-align: center;
+					width: 100%;
+					}
+					/* User Card */
+					.card {
+						border: 2px solid #BEBEBE;
+						border-radius: 10px;
+						padding: .5rem;
+						min-height: 150px;
+						max-height: 300px;
+						overflow: scroll;
+					}
+					img {
+						margin-top: 15px;
+						border-radius: 10px;
+						display: block;
+					}		
+					.hide {
+						display: none;
+					}
+					.show {
+						font-size: large;
+						padding-left:0;
+						display: block;
+					}
+					.btn {
+						border: none;
+						height: 36px;
+						width: 140px;
+						padding: 6px 18px;
+						border-radius: 20px;
+						background: #007AA3;
+						color: white;
+						cursor: pointer;
+						transition: 0.3s;
+					}
+					.btn:hover{
+						background: #005E7D;
+						opacity: 1;
+					}
+					.italic{
+						font-style: oblique;
+						text-decoration: underline #FF3028;
+					}
+		</style>
+					
+					
+		
+					<div class="container">
+						<div class="cards">
+								<div class="card">
+									<button class="btn">Customer Phone</button>
+									<p class="hide"> ${this.ani}</p>
+								</div>
+
+								<div class="card">
+									<button class="btn">Cad</button>
+									<p class="hide"> value: ${this.myFunc()}</p>
+								</div>
+
+								<div class="card">
+									<button class="btn">Details</button>
+									<p class="hide"> ${JSON.stringify(this.details)}</p>
+								</div>
+							
+								<div class="card">
+									<button class="btn">Wrap</button>
+									<p class="hide"> ${JSON.stringify(this.wrap)}</p>
+								</div>
+
+								<div class="card">
+									<button class="btn">External API</button>
+									<img src=${this.avatar} class="hide" alt="image">
+								</div>
+
+								<div class="card">
+									<button class="btn">From Layout</button>
+									<h3 class="hide"> ${this.name}</h3>
+								</div>
+
+								<div class="card">
+									<button id="hold" class="btn">Media Info</button>
+									<p class="hide"> ${JSON.stringify(this.mediaInfo())}</p>
+								</div>
+
+								<div class="card">
+									<button class="btn">Participants</button>
+									<p class="hide"> ${this.activeParticipants()}</p>
+								</div>
+
+								<div class="card">
+									<button class="btn">Access Token</button>
+									<p class="hide"><span class="italic">Auth Token for SSO:</span> \n${this.token}</p>
+								</div>
+						
+								<div class="card">
+									<button class="btn">More</button>
+									<p class="hide"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet expedita beatae totam, soluta mollitia fuga architecto maiores modi obcaecati nam, nemo, culpa sequi. Doloremque optio, minima tempora pariatur nulla provident
+									</p>
+								</div>
+						</div>
+					</div>
+		`;
+
+    // **STEP (4)** //
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    // toggle
+    Array.from(this.shadowRoot.querySelectorAll("div.card")).forEach(e => {
+      e.addEventListener("click", function (e) {
+        let target = e.target.nextSibling.nextSibling;
+        target.classList.toggle("hide");
+      });
+    });
+  }
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    if (attrName === "name") {
+      Array.from(this.shadowRoot.querySelectorAll("h3")).forEach(e => {
+        e.innerText = this.getAttribute("name");
+      });
+    } else if (attrName === "avatar") {
+      Array.from(this.shadowRoot.querySelectorAll("img")).forEach(e => {
+        e.href = this.getAttribute("avatar");
+      });
+    } else if (attrName === "darkmode") {
+      const darkMode = this.getAttribute("darkmode");
+      if (darkMode === "true") {
+        Array.from(this.shadowRoot.querySelectorAll(".card")).forEach(e => {
+          e.style.background = "#000";
+          e.style.color = "#fff";
+        });
+      } else {
+        Array.from(this.shadowRoot.querySelectorAll(".card")).forEach(e => {
+          e.style.background = "#fff";
+          e.style.color = "#000";
+        });
+      }
+    }
+  }
+  static get observedAttributes() {
+    return ["name", "darkmode", "avatar"];
+  }
+}
+window.customElements.define("info-card", InfoCard);
