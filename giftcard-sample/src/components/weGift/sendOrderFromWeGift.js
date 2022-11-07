d41fa397600ba49830853e11ecadff37c8876062
Niko Theo <niko.th@gmail.com>
2022-11-07 10:15:49 -0800

    giftcard-sample

---
+++ giftcard-sample/src/components/weGift/sendOrderFromWeGift.js
@@ -0,0 +1,20 @@
+export async function sendOrderFromWeGift(raw) {
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: raw,
+    redirect: "follow"
+  };
+
+  try {
+    // let response = await fetch("http://localhost:3005/wegift", requestOptions);
+    let response = await fetch("https://saproxy.herokuapp.com/wegift", requestOptions);
+    let result = await response.text();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
