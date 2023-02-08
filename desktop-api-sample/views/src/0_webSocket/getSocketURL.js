e393ccad50b1e5dbaa6efb1f2b98527541f40156
sdoddali <sdoddali@cisco.com>
2023-02-08 09:44:47 -0500

    fixed idle state issue

--- desktop-api-sample/views/src/0_webSocket/getSocketURL.js
+++ desktop-api-sample/views/src/0_webSocket/getSocketURL.js
@@ -1,27 +1,27 @@
 export async function getSocketURL(token) {
-  const force = true;
-  const isKeepAliveEnabled = true;
-  const allowMultiLogin = true;
-  const clientType = "AgentDesktopMessage";
-  let raw = JSON.stringify({
-    force,
-    isKeepAliveEnabled,
-    allowMultiLogin,
-    clientType
-  });
-  try {
-    const posts = await fetch(`https://api.wxcc-us1.cisco.com/v1/notification/subscribe?force=true`, {
-      method: "POST",
-      headers: {
-        // Authorization: `Bearer ${token.ccAccessToken}`,
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json"
-      },
-      body: raw
+    const force = true;
+    const isKeepAliveEnabled = false;
+    const allowMultiLogin = true;
+    const clientType = "AgentDesktopMessage";
+    let raw = JSON.stringify({
+        force,
+        isKeepAliveEnabled,
+        allowMultiLogin,
+        clientType
     });
-    const response = await posts.json();
-    return response.webSocketUrl;
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const posts = await fetch(`https://api.wxcc-us1.cisco.com/v1/notification/subscribe?force=true`, {
+            method: "POST",
+            headers: {
+                // Authorization: `Bearer ${token.ccAccessToken}`,
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            body: raw
+        });
+        const response = await posts.json();
+        return response.webSocketUrl;
+    } catch (error) {
+        console.log(error);
+    }
 }
