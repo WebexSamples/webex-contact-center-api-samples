c88268927924ffef0efe5eb82118f7ba6cbf029d
jiwyatt <jiwyatt@cisco.com>
2023-09-20 10:47:53 -0500

    apply rate limiter to all requests

--- callback-samples/callback-sample-nodejs-login/server.js
+++ callback-samples/callback-sample-nodejs-login/server.js
@@ -31,6 +31,20 @@ app.use(express.json());
 
 app.use(express.static('./dist'));
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
+
+
+
+
 app.get('/', (req, res) => {
   /*
    * This basic endpoint only shows you the access token if existing in memory.
