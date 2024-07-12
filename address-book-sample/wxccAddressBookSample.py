85acc263b283a773e3115f0e580a289601026002
jozanini <jozanini@cisco.com>
2024-07-12 15:08:46 -0700

    post autopep run

--- address-book-sample/wxccAddressBookSample.py
+++ address-book-sample/wxccAddressBookSample.py
@@ -6,8 +6,11 @@ address_book_id = 'your_address_book_id'
 
 # Function to get a list of random users from the randomuser.me API
 # This could be data pulled from a CRM or Azure AD, etc.
+
+
 def get_random_user_list():
-    response = requests.get('https://randomuser.me/api/?results=10&inc=name,phone&nat=us&noinfo')
+    response = requests.get(
+        'https://randomuser.me/api/?results=10&inc=name,phone&nat=us&noinfo')
     if response.status_code == 200:
         data = response.json()
         return data
@@ -15,6 +18,8 @@ def get_random_user_list():
         return None
 
 # Function to populate the payload with the data from the random user list
+
+
 def populate_payload(response_data):
     # Initialize the payload with an empty 'items' list
     payload = {"items": []}
@@ -38,6 +43,8 @@ def populate_payload(response_data):
     return payload
 
 # Function to update the address book with the new data via the WxCC API
+
+
 def update_address_book(data):
     url = f'https://api.wxcc-us1.cisco.com/organization/{org_id}/address-book/{address_book_id}/entry/bulk'
     headers = {
@@ -48,5 +55,6 @@ def update_address_book(data):
 
     response = requests.request("POST", url, json=data, headers=headers)
     return response
+
 
 print(update_address_book(populate_payload(get_random_user_list())))
